f0d1001fca2f02663addc256186290b51e106d54
jonsteingass <steingass.5@wright.edu>
2018-05-10 11:33:59 -0400

    Added empty service for vision based positioning algorithm

---
+++ src/Services/VisionBasedPositionService.cpp
@@ -0,0 +1,128 @@
+// ===============================================================================
+// Authors: AFRL/RQQA
+// Organization: Air Force Research Laboratory, Aerospace Systems Directorate, Power and Control Division
+// 
+// Copyright (c) 2017 Government of the United State of America, as represented by
+// the Secretary of the Air Force.  No copyright is claimed in the United States under
+// Title 17, U.S. Code.  All Other Rights Reserved.
+// ===============================================================================
+
+/* 
+ * File:   VisionBasedPositionService.cpp
+ * Authors: Jon
+ *
+ * Created on March 17, 2017, 5:55 PM
+ *
+ * <Service Type="VisionBasedPositionService" VehicleId="0"/>
+ * 
+ */
+
+// include header for this service
+#include "VisionBasedPositionService.h"
+
+#include "afrl/cmasi/EntityState.h"
+#include "uxas/projects/teas/VisionBasedPosition.h"
+
+#include <iostream>     // std::cout, cerr, etc
+
+// convenience definitions for the option strings
+#define VehicleId "VehicleId"
+
+// namespace definitions
+namespace uxas  // uxas::
+{
+namespace service   // uxas::service::
+{
+
+// this entry registers the service in the service creation registry
+VisionBasedPositionService::ServiceBase::CreationRegistrar<VisionBasedPositionService>
+VisionBasedPositionService::s_registrar(VisionBasedPositionService::s_registryServiceTypeNames());
+
+// service constructor
+VisionBasedPositionService::VisionBasedPositionService()
+: ServiceBase(VisionBasedPositionService::s_typeName(), VisionBasedPositionService::s_directoryName()) { };
+
+// service destructor
+VisionBasedPositionService::~VisionBasedPositionService() { };
+
+
+//set up service based on xml attributes and subscribe to messages here
+bool VisionBasedPositionService::configure(const pugi::xml_node& ndComponent)
+{
+    bool isSuccess(true);
+
+    if (!ndComponent.attribute(VehicleId).empty())
+    {
+        m_vehicleId = ndComponent.attribute(VehicleId).as_int();
+    }
+
+    // subscribe to messages::
+    // ENTITY STATES
+    addSubscriptionAddress(afrl::cmasi::EntityState::Subscription);
+    std::vector< std::string > childstates = afrl::cmasi::EntityStateDescendants();
+    for (auto child : childstates)
+        addSubscriptionAddress(child);
+    return (isSuccess);
+}
+
+//send message when intiialized
+bool VisionBasedPositionService::initialize()
+{
+    // perform any required initialization before the service is started
+    std::cout << "*** INITIALIZING:: Service[" << s_typeName() << "] Service Id[" << m_serviceId << "] with working directory [" << m_workDirectoryName << "] *** " << std::endl;
+    
+    return (true);
+}
+
+//send message when the service is starting
+bool VisionBasedPositionService::start()
+{
+    // perform any actions required at the time the service starts
+    std::cout << "*** STARTING:: Service[" << s_typeName() << "] Service Id[" << m_serviceId << "] with working directory [" << m_workDirectoryName << "] *** " << std::endl;
+    
+    return (true);
+};
+
+//print statement when service is terminating. Do some clean up here
+bool VisionBasedPositionService::terminate()
+{
+    // perform any action required during service termination, before destructor is called.
+    std::cout << "*** TERMINATING:: Service[" << s_typeName() << "] Service Id[" << m_serviceId << "] with working directory [" << m_workDirectoryName << "] *** " << std::endl;
+    
+    return (true);
+}
+
+//The meat of a service. This is where the messages are digested and other stuff is handled
+bool VisionBasedPositionService::processReceivedLmcpMessage(std::unique_ptr<uxas::communications::data::LmcpMessage> receivedLmcpMessage)
+{
+    std::shared_ptr<avtas::lmcp::Object> messageObject = receivedLmcpMessage->m_object;
+    
+    auto entityState = std::dynamic_pointer_cast<afrl::cmasi::EntityState>(messageObject);
+    bool isReadyToSend = false;
+    auto positionMessage = std::shared_ptr<uxas::projects::teas::VisionBasedPosition>(new uxas::projects::teas::VisionBasedPosition());
+    
+    if(entityState && entityState->getID() == m_vehicleId)
+    {
+        //get IMU data for vision-based navigation algorithm?
+        //Check out what the entity state contains in the MDMs (OpenUxAS/doc/LMCP/index.html and look for entitystate)
+    }
+    
+    if(isReadyToSend)
+    {
+        //positionMessage->setLatitude(someLatVal);
+        //positionMessage->setLongitude(someLonVal);
+        
+        //can set variance of direction if in covariance matrix (diagonal vals?). Otherwise its not the end of the world
+        //positionMessage->setVarianceX(0.0);
+        //positionMessage->setVarianceY(0.0);
+        //positionMessage->setVarianceZ(0.0);
+        
+        //Send the message
+    }
+    
+    
+    return false;
+}
+
+}; //namespace service
+}; //namespace uxas
---
+++ src/Services/VisionBasedPositionService.h
@@ -0,0 +1,187 @@
+// ===============================================================================
+// Authors: AFRL/RQQA
+// Organization: Air Force Research Laboratory, Aerospace Systems Directorate, Power and Control Division
+// 
+// Copyright (c) 2017 Government of the United State of America, as represented by
+// the Secretary of the Air Force.  No copyright is claimed in the United States under
+// Title 17, U.S. Code.  All Other Rights Reserved.
+// ===============================================================================
+
+/* 
+ * File:   VisionBasedPositionService.h
+ * Author: steve
+ *
+ * Created on March 17, 2017, 5:55 PM
+ */
+
+#ifndef UXAS_VISIONBASEDPOSITIONSERVICE_H
+#define UXAS_VISIONBASEDPOSITIONSERVICE_H
+
+
+
+#include "ServiceBase.h"
+#include "CallbackTimer.h"
+#include "TypeDefs/UxAS_TypeDefs_Timer.h"
+
+namespace uxas {
+    namespace service {
+
+        /*! \class VisionBasedPositionService
+            \brief This is a basic service that can be used as a template when 
+         * constructing new services.
+
+         * 
+         * 
+         *  @par To add a new service:
+         * <ul style="padding-left:1em;margin-left:0">
+         * <li>Make copies of the source and header files of this template.</li>
+         * <li>Search for the string VisionBasedPositionService and Replace it with the new 
+         * service name.</li>
+         * <li>Change the unique include guard entries, in the header file, i.e. 
+         * "UXAS_VISIONBASEDPOSITIONSERVICE_H" to match the new service name</li>
+         * <li> include the new service header file in ServiceManager.cpp</li>
+         * <li> add a dummy instance of the new service in ServiceManager.cpp, e.g.
+         * {auto svc = uxas::stduxas::make_unique<uxas::service::MyNewService>();} 
+         * Note: this is required to link the new service in when building UxAS</li>
+         *  
+         * </ul> @n
+         * 
+         * Configuration String: <Service Type="VisionBasedPositionService" VehicleId="0" />
+         * 
+         * Options:
+         *  - VehicleId - The Id of the vehicle to be sent in the VisionBasedPosition message. Used to only use entity state of vehicle navigating
+         * 
+         * Subscribed Messages:
+         *  - afrl::cmasi::EntityState
+         *		This message contains the current state of the vehicle. For our purposes this will be cast to an AirVehicleState.
+         *		Refer to the details of AirVehicleState and EntityState for more information.
+         *		Details found in the following path in the OpenUxAS directory: doc/LMCP/index.html
+         *		Contains:
+         *				-ID:			   A unique ID for this entity. IDs should be greater than zero (int64)[None]
+         *
+         *				-u:				   Velocity in the body x-direction (positive out nose) (real32)[meter/sec]
+         *				
+         *				-v:				   Velocity in the body y-direction (positive out right wing) (real32)[meter/sec]
+         *				
+         *				-w:				   Velocity in the body z-direction (positive downward) (real32)[meter/sec]
+         *				
+         *				-udot:			   Accelaration in the body x-direction (positive out nose) (real32)[meter/sec/sec]
+         *				
+         *				-vdot:			   Acceleration in the body y-direction (positive out right wing) (real32)[meter/sec/sec]
+         *				
+         *				-wdot:			   Acceleration in the body z-direction (positive downward) (real32)[meter/sec/sec]
+         *				
+         *				-Heading:		   Angle between true North and the projection of the body x-axis in the North-East plane (real32)[degree]
+         *				
+         *				-Pitch:			   Pitch of vehicle around body y-axis (positive upwards) (real32)[degree]
+         *				
+         *				-Roll:			   Roll angle of the vehicle around body x-axis (positive right wing down) (real32)[degree]
+         *				
+         *				-p:				   roll-rate of vehicle (angular velocity around body x-axis). Positive right-wing down (real32)[degree/sec]
+         *				
+         *				-q:				   pitch rate of the vehicle (angular velocity around body y-axis). Positive nose-up. (real32)[degree/sec]
+         *				
+         *				-r:				   yaw rate of the vehicle (angular velocity around body z-axis). Positive nose right. (real32)[degree/sec]
+         *				
+         *				-Course:		   Course/Groundtrack angle of the entity referenced to true North (real32)[degrees]
+         *				
+         *				-GroundSpeed:	   Currenty entity ground speed (real32)[meter/sec]
+         *				
+         *				-Location:		    The perceived entity location. A valid EntityState must include location (Location3D)[None]
+         *				
+         *				-EnergyAvailable:   The available energy remaining, expressed in terms of the percentage of maximum capacity (real32)[%]
+         *				
+         *				-ActualEnergyRate:  The consumption rate of available energy, expressed in terms of the percentage of maximum capacity used per second (real32)[%/sec]
+         *				
+         *				-PayloadStateList:  A list of states for any onboard payloads (PayloadState[])[None]
+         *				
+         *				-CurrentWaypoint:   The ID of the current waypoint. Only valid if the vehicle is in waypoint following mode. (int64)[None]
+         *				
+         *				-Mode:				The current mode for this vehicle. (NavigationMode)[None]
+         *				
+         *				-AssociatedTasks:	Tasks that this entity is currently executing. The task number should coincide with the task number in the task request. (int64)[None]
+         *				
+         *				-Time:				Timestamp of this data. Time datum is defined by the application, but unless otherwise specified is milliseconds since Jan 1st 1970. (int64)[millisecond]
+         *				
+         *				-Info:				A list that maps keys to values for the inclusion of extra, custom information about this entity (KeyValuePair[])[None]
+         * 
+         * Sent Messages:
+         *  - uxas::projects::teas::VisionBasedNavPosition
+         *          This message will be sent to communicate to other services the position of the vehicle as calculated by the vision based algorithm.
+         * 
+         * 
+         */
+
+        class VisionBasedPositionService : public ServiceBase {
+        public:
+
+            /** \brief This string is used to identify this service in XML configuration
+             * files, i.e. Service Type="VisionBasedPositionService". It is also entered into
+             * service registry and used to create new instances of this service. */
+            static const std::string&
+            s_typeName() {
+                static std::string s_string("VisionBasedPositionService");
+                return (s_string);
+            };
+
+            static const std::vector<std::string>
+            s_registryServiceTypeNames() {
+                std::vector<std::string> registryServiceTypeNames = {s_typeName()};
+                return (registryServiceTypeNames);
+            };
+
+            /** \brief If this string is not empty, it is used to create a data 
+             * directory to be used by the service. The path to this directory is
+             * accessed through the ServiceBase variable m_workDirectoryPath. */
+            static const std::string&
+            s_directoryName() {
+                static std::string s_string("");
+                return (s_string);
+            };
+
+            static ServiceBase*
+            create() {
+                return new VisionBasedPositionService;
+            };
+
+            VisionBasedPositionService();
+
+            virtual
+            ~VisionBasedPositionService();
+
+        private:
+
+            static
+            ServiceBase::CreationRegistrar<VisionBasedPositionService> s_registrar;
+
+            /** brief Copy construction not permitted */
+            VisionBasedPositionService(VisionBasedPositionService const&) = delete;
+
+            /** brief Copy assignment operation not permitted */
+            void operator=(VisionBasedPositionService const&) = delete;
+
+            bool
+            configure(const pugi::xml_node& serviceXmlNode) override;
+
+            bool
+            initialize() override;
+
+            bool
+            start() override;
+
+            bool
+            terminate() override;
+
+            bool
+            processReceivedLmcpMessage(std::unique_ptr<uxas::communications::data::LmcpMessage> receivedLmcpMessage) override;
+
+
+        private:
+          int m_vehicleId{0};  
+        };
+
+    }; //namespace service
+}; //namespace uxas
+
+#endif /* UXAS_VISIONBASEDPOSITIONSERVICE_H */
+
